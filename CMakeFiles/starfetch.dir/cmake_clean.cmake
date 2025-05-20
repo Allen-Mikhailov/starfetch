07af079c97aad0e6f2e7803695e1680f6a40b6f2
Allen Mikhailov <mikhailovallen@gmail.com>
2025-05-20 01:57:59 -0500

    Basic CMake working

---
+++ CMakeFiles/starfetch.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/starfetch.dir/starfetch.c.o"
+  "CMakeFiles/starfetch.dir/starfetch.c.o.d"
+  "starfetch"
+  "starfetch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/starfetch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/starfetch.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for starfetch.
+# This may be replaced when dependencies are built.
